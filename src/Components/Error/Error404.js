fbf134fb36250457c46c89cac9038616dc92442b
Rgarcia <rodrigo.seb.garcia@gmail.com>
2023-03-23 21:30:17 -0300

    FixedCssBugs

--- src/Components/Error/Error404.js
+++ src/Components/Error/Error404.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+
 const Error404 = () =>{
     return(
 
-        <div className="container">
-            <div className="row my-5">
-                <div className="col text-center">
+        <div className={`container-fluid`}>
+            <div className="d-flex justify-content-center align-items-center">
+                <div className="text-center">
                     <h1>Error <b>404</b></h1>
                     <h3>La pagina no existe</h3>
                 </div>
